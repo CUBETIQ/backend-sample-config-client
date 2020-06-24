b33763fcff6f89bf7f73c42dec3dfc3a5243c5d2
Sambo Chea <sombochea@cubetiqs.com>
2020-06-24 09:59:10 +0700

    Add first sample config client

---
+++ src/main/java/com/cubetiqs/example/configclient/BackendConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.cubetiqs.example.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cubetiqs/example/configclient/BackendConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubetiqs.example.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
